1d24203064e68d1145a72d7ffadf260c3426239f
Mingsicogi <codedoctor119@gmail.com>
2019-07-05 03:35:13 +0900

    + Future

--- src/FutureEx.java
+++ src/FutureEx.java
@@ -1,19 +1,92 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Objects;
+import java.util.concurrent.*;
 
 public class FutureEx {
 
-	public static void main(String[] args) {
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    interface ExceptionCallBack {
+        void onError(Throwable t);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String>{
+
+        SuccessCallback sc;
+        ExceptionCallBack ec;
+
+        @Override
+        protected void done() {
+            try {
+                sc.onSuccess(get());
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc, Throwable ec) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc);
+            this.ec = Objects.requireNonNull(ec);
+        }
+    }
+
+	// Future
+	// Callback
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		ExecutorService es = Executors.newCachedThreadPool();
 
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Hello");
+		CallbackFutureTask f = new CallbackFutureTask(() -> {
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName() + " : " + "Async");
 
-		System.out.println("Exit");
+            return "HELLO";
+
+        }, System.out::println, e -> System.out.println("Error : "));
+
+		es.execute(f);
+		es.shutdown();
+
+//		Future<String> f = es.submit(() -> {
+//
+//			Thread.sleep(2000);
+//			System.out.println(Thread.currentThread().getName() + " : " + "Async");
+//
+//			return "HELLO";
+//		});
+
+//		System.out.println(f.isDone());
+//		Thread.sleep(2100);
+//		System.out.println(Thread.currentThread().getName() + " : " + "Exit");
+//
+//		System.out.println(f.isDone());
+//		System.out.println(f.get()); // blocking status...
+
+
+//        FutureTask<String> f = new FutureTask<>(() -> {
+//            Thread.sleep(2000);
+//            System.out.println(Thread.currentThread().getName() + " : " + "Async");
+//
+//            return "HELLO";
+//        }) {
+//            @Override
+//            protected void done() {
+//                try{
+//                    System.out.println(get());
+//                }catch (InterruptedException e){
+//                    e.printStackTrace();
+//                }
+//                catch (ExecutionException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//
+//        es.execute(f);
+
 	}
 }
